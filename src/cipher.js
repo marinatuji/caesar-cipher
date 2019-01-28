d41e184b3cf0f3285c7efaad11ad1c688582ef12
marinatuji <mtujimura@gmail.com>
2019-01-28 02:34:15 -0200

    Implemento da funcao encode e decode simple version

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,29 @@
-window.cipher = {
-  // ... 
-};
+/* não usar window
+*/
+
+function encode(form) {
+    let offset = parseInt(form.kcipher.value);
+    let msgCipher = form.mcipher.value.toUpperCase();
+    let encodedMsg = [];
+
+    for (i = 0; i < msgCipher.length; i++) {
+        let indexAsc = msgCipher.charCodeAt(i);
+        let offsetIndex = ((indexAsc - 65 + offset) % 26) + 65;
+        encodedMsg.push(String.fromCharCode(offsetIndex));
+    }
+    alert(encodedMsg.join(""));
+}
+
+function decode(form) {
+ let offset = parseInt(form.kdecipher.value);
+ let msgDecipher = form.mdecipher.value.toUpperCase();
+ let decodedMsg = [];
+
+ for(i = 0; i < msgDecipher.length; i++) {
+    let indexAsc = msgDecipher.charCodeAt(i);
+    let originalIndex = ((indexAsc - 65 - (((offset % 26) + 26)) %26)) + 65;
+    decodedMsg.push(String.fromCharCode(originalIndex));
+
+}
+alert(decodedMsg.join(""));
+} 

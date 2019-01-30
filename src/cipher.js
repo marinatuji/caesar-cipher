68f9e0f61838a985468d981f9c3bf8fc8f95057e
marinatuji <mtujimura@gmail.com>
2019-01-30 13:10:26 -0200

    implementação do javascript e html com revisão de melhores praticas

--- src/cipher.js
+++ src/cipher.js
@@ -1,29 +1,43 @@
-/* não usar window
-*/
-
-function encode(form) {
+//CHECAR O REQUISITO DO PARAMENTRO DA FUNÇÃO reduzir as "acões" da função o máximo possível
+function buildMsgCipher(form) {
     let offset = parseInt(form.kcipher.value);
-    let msgCipher = form.mcipher.value.toUpperCase();
-    let encodedMsg = [];
+    let string = form.mcipher.value;
+    document.getElementById("show-msg").innerHTML = encode(offset, string);
+}
 
-    for (i = 0; i < msgCipher.length; i++) {
-        let indexAsc = msgCipher.charCodeAt(i);
-        let offsetIndex = ((indexAsc - 65 + offset) % 26) + 65;
-        encodedMsg.push(String.fromCharCode(offsetIndex));
+function buildMsgDecipher(form){
+    let offset = parseInt(form.kcipher.value);
+    let string = form.mcipher.value;
+    document.getElementById("show-msg").innerHTML = decode(offset, string);
+}
+
+function encode(offset, string) {
+    let msgEncoded = "";
+    for (i = 0; i < string.length; i++) {
+        let indexAsc = string.charCodeAt(i);
+        if (indexAsc >= 65 && indexAsc <= 90) {
+            let offsetIndex = ((indexAsc - 65 + offset) % 26) + 65;
+            msgEncoded += String.fromCharCode(offsetIndex);
+        } else if (indexAsc >= 97 && indexAsc <= 122) {
+            let offsetIndex = ((indexAsc - 97 + offset) % 26) + 97;
+            msgEncoded += String.fromCharCode(offsetIndex);
+        } else {
+            msgEncoded += String.fromCharCode(indexAsc);
+        }
     }
-    alert(encodedMsg.join(""));
+    return msgEncoded;
 }
 
 function decode(form) {
- let offset = parseInt(form.kdecipher.value);
- let msgDecipher = form.mdecipher.value.toUpperCase();
- let decodedMsg = [];
+    let offset = parseInt(form.kdecipher.value);
+    let msgDecipher = form.mdecipher.value.toUpperCase();
+    let msgDecoded = "";
 
- for(i = 0; i < msgDecipher.length; i++) {
-    let indexAsc = msgDecipher.charCodeAt(i);
-    let originalIndex = ((indexAsc - 65 - (((offset % 26) + 26)) %26)) + 65;
-    decodedMsg.push(String.fromCharCode(originalIndex));
+    for (i = 0; i < msgDecipher.length; i++) {
+        let indexAsc = msgDecipher.charCodeAt(i);
+        let originalIndex = ((indexAsc - 65 - (((offset % 26) + 26)) % 26)) + 65; //funcao da Ma
+        msgDecoded += String.fromCharCode(originalIndex);
 
+    }
+    alert(msgDecoded);
 }
-alert(decodedMsg.join(""));
-} 
---
+++ src/tests.js
@@ -0,0 +1,9 @@
+//pensar nos testes da sua aplicação javascript
+
+function test (){
+    let result = encode(1,"zaza");
+    if(result === "azaz"){
+        console.log("ok!")
+    }
+    console.log("deu erro")
+}
